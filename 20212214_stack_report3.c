6bb6e2814bf69c03f31c141de366e2eb4a505add
20212214-cyber <20212214@vision.hoseo.edu>
2026-03-31 14:31:33 +0900

    Implement stack-based infix to postfix conversion

---
+++ 20212214_stack_report3.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+typedef struct {
+    int data[MAX];
+    int top;
+} Stack;
+
+void initStack(Stack* s) {
+    s->top = -1;
+}
+
+int isEmpty(Stack* s) {
+    return s->top == -1;
+}
+
+void push(Stack* s, int value) {
+    if (s->top < MAX - 1) {
+        s->data[++(s->top)] = value;
+    }
+}
+
+int pop(Stack* s) {
+    if (isEmpty(s)) return 0;
+    return s->data[(s->top)--];
+}
+
+int peek(Stack* s) {
+    if (isEmpty(s)) return 0;
+    return s->data[s->top];
+}
+
+int precedence(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return -1;
+}
+
+int evaluatePostfix(char* postfix) {
+    Stack s;
+    initStack(&s);
+
+    for (int i = 0; postfix[i] != '\0'; i++) {
+        char ch = postfix[i];
+
+        if (isdigit(ch)) {
+            push(&s, ch - '0');
+        }
+        else {
+            int b = pop(&s);
+            int a = pop(&s);
+            switch (ch) {
+            case '+': push(&s, a + b); break;
+            case '-': push(&s, a - b); break;
+            case '*': push(&s, a * b); break;
+            case '/': push(&s, a / b); break;
+            }
+        }
+    }
+    return pop(&s);
+}
+
+int infixToPostfix(char* infix, char* postfix) {
+    Stack s;
+    initStack(&s);
+    int j = 0;
+
+    for (int i = 0; infix[i] != '\0'; i++) {
+        char ch = infix[i];
+
+        if (isdigit(ch) || isalpha(ch)) {
+            postfix[j++] = ch;
+        }
+        else if (ch == '(') {
+            push(&s, ch);
+        }
+        else if (ch == ')') {
+            while (!isEmpty(&s) && peek(&s) != '(') {
+                postfix[j++] = pop(&s);
+            }
+            pop(&s);
+        }
+        else {
+            while (!isEmpty(&s) && precedence(peek(&s)) >= precedence(ch)) {
+                postfix[j++] = pop(&s);
+            }
+            push(&s, ch);
+        }
+    }
+
+    while (!isEmpty(&s)) {
+        postfix[j++] = pop(&s);
+    }
+    postfix[j] = '\0';
+    return evaluatePostfix(postfix);
+}
+
+int main(void) {
+    char infix[MAX];
+    char postfix[MAX];
+    int result;
+
+    printf(":중위표기식 입력 : ");
+    if (scanf_s("%s", infix) == 1) {
+        result = infixToPostfix(infix, postfix);
+
+        printf("후위표기식 : %s\n", postfix);
+        printf("후위표기식 계산 결과 : %d\n", result);
+    }
+
+    return 0;
+}
