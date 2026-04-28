71b6f99ac3c895d41de31c40d51e4e90f3a7f4fa
20212214-cyber <20212214@vision.hoseo.edu>
2026-04-28 12:15:08 +0900

    Implement singly linked list operations in C

---
+++ 20212214_SingleLL_report1.c
@@ -0,0 +1,161 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+Node* insertFirst(Node* head, int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = head;
+    return newNode;
+}
+
+Node* insertLast(Node* head, int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (head == NULL) return newNode;
+
+    Node* current = head;
+    while (current->next != NULL) current = current->next;
+    current->next = newNode;
+    return head;
+}
+
+void insertAfterValue(Node* head, int targetValue, int newData) {
+    Node* current = head;
+    while (current != NULL) {
+        if (current->data == targetValue) {
+            Node* newNode = (Node*)malloc(sizeof(Node));
+            newNode->data = newData;
+            newNode->next = current->next;
+            current->next = newNode;
+            return;
+        }
+        current = current->next;
+    }
+}
+
+Node* deleteHead(Node* head) {
+    if (head == NULL) return NULL;
+    Node* temp = head;
+    head = head->next;
+    free(temp);
+    return head;
+}
+
+Node* deleteLast(Node* head) {
+    if (head == NULL) return NULL;
+    if (head->next == NULL) {
+        free(head);
+        return NULL;
+    }
+    Node* current = head;
+    while (current->next->next != NULL) current = current->next;
+    free(current->next);
+    current->next = NULL;
+    return head;
+}
+
+Node* deleteNode(Node* head, int targetValue) {
+    if (head == NULL) return NULL;
+    if (head->data == targetValue) return deleteHead(head);
+
+    Node* current = head;
+    while (current->next != NULL && current->next->data != targetValue) {
+        current = current->next;
+    }
+
+    if (current->next != NULL) {
+        Node* temp = current->next;
+        current->next = temp->next;
+        free(temp);
+    }
+    return head;
+}
+
+void printList(Node* head) {
+    Node* current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+int searchValue1(Node* head, int val) {
+    Node* current = head;
+    int pos = 1;
+    while (current != NULL) {
+        if (current->data == val) return pos;
+        current = current->next;
+        pos++;
+    }
+    return -1;
+}
+
+void searchValue2(Node* head, int val) {
+    Node* prev = NULL;
+    Node* current = head;
+
+    while (current != NULL) {
+        if (current->data == val) {
+            printf("값 %d를 찾았습니다.\n", val);
+            if (prev != NULL) printf("앞 노드: %d\n", prev->data);
+            else printf("앞 노드가 없습니다.\n");
+
+            if (current->next != NULL) printf("뒤 노드: %d\n", current->next->data);
+            else printf("뒤 노드가 없습니다.\n");
+            return;
+        }
+        prev = current;
+        current = current->next;
+    }
+    printf("값 %d를 리스트에서 찾을 수 없습니다.\n", val);
+}
+
+void freeList(Node* head) {
+    Node* current = head;
+    while (current != NULL) {
+        Node* temp = current;
+        current = current->next;
+        free(temp);
+    }
+}
+
+int main() {
+    Node* head = NULL;
+
+    head = insertFirst(head, 30); 
+    head = insertFirst(head, 20); 
+    head = insertFirst(head, 10); 
+    head = insertLast(head, 70); 
+    head = insertLast(head, 80); 
+    head = insertLast(head, 90); 
+    insertAfterValue(head, 30, 40); 
+    insertAfterValue(head, 40, 50); 
+    insertAfterValue(head, 50, 60); 
+
+    printList(head);
+
+    int val1 = 30;
+    int pos = searchValue1(head, val1);
+    printf("%d의 위치는 %d번째\n", val1, pos);
+
+    int val2 = 50;
+    searchValue2(head, val2);
+
+    head = deleteHead(head);
+    head = deleteLast(head);
+    head = deleteNode(head, 40);
+
+    printList(head);
+
+    freeList(head);
+
+    return 0;
+}
